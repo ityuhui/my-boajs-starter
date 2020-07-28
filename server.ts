ee8bf8d4a4760a32ec473424d15cd7909b8c6115
Hui Yu <ityuhui@gmail.com>
2020-07-28 23:05:29 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   server.js 	modified:   server.ts

--- server.ts
+++ server.ts
@@ -1,19 +1,14 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
-import * as bodyParser from "koa-bodyparser";
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/*', async (ctx) => {
+router.get('/', async (ctx) => {
     ctx.body = 'Hello World!';
 });
 
-app
-  .use(bodyParser())
-  .use(router.routes())
-  .use(router.allowedMethods());
-
+app.use(router.routes());
 app.listen(3000);
 
 console.log('Server running on port 3000');
